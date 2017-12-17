bf184e93ffb068f0d0f1479e0bac3de4da015811
jzh <1092431698@qq.com>
2017-12-17 19:52:05 +0800

    change client processor place

---
+++ JGame/JProcessor/ClientProcessors/JProcessorSignIn.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+
+namespace JGame.Processor
+{
+	using JGame.StreamObject;
+	using JGame;
+	using JGame.Network;
+	using JGame.Data;
+	using JGame.Logic;
+	using JGame.Processor;
+	using JGame.Log;
+	public class JProcesserSignInSet :  IProcessor
+	{
+		public void run(IDataSet dataSet)
+		{
+			IStreamObj obj = dataSet.getData (JObjectType.sign_in);
+			JObj_SignIn signInObj = obj as JObj_SignIn;
+			if (signInObj == null)
+				return;
+			SendToServer (signInObj);
+		}
+
+		#region 私有方法
+		protected bool SendToServer(JObj_SignIn obj)
+		{
+			try {
+				JNetworkDataOperator.SendData(JPacketType.npt_signin_req, obj);
+				return true;
+			} catch (Exception e) {
+				JLog.Debug ("发送数据失败");
+				JLog.Error (e.Message);
+				return false;
+			}
+		}
+		#endregion
+	}
+
+	public class JProcesserSignInGet: IProcessor
+	{
+		public delegate void ToSignIn();
+		public ToSignIn toSignIn;
+
+		public void run(IDataSet dataSet)
+		{
+			IStreamObj obj = dataSet.getData (JObjectType.sign_in_ret);
+			if (null == obj || null == (obj as JObj_SignRet))
+				JLog.Error ("JProcesserSignInGet : obj is empty!");
+
+			if ((obj as JObj_SignRet).Result == false)
+				JLog.Info ("Received JObj_SignRet but account and code is not registed!");
+			//todo:...remind to regist
+			else
+				if (null != toSignIn)
+					toSignIn ();
+		}
+	}
+}
--- Processor/AccountRegister.cs
+++ Processor/AccountRegister.cs
@@ -10,13 +10,14 @@ using JGame;
 using JGame.Network;
 using JGame.Data;
 using JGame.Logic;
+using JGame.Data;
 using JGame.Processor;
 using JGame.Log;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class JProcesserSignIn :  MonoBehaviour
+public class AccountRegister :  MonoBehaviour
 {
 	public Slider _progress = null; //进度条
 	public Text _progress_text = null;//进度条文字
@@ -28,7 +29,7 @@ public class JProcesserSignIn :  MonoBehaviour
 		processor.toSignIn += SignIn;
 		JLogicHelper.registerProcessor (JPacketType.npt_signin_ret,  processor,false);
 	}
-		
+
 	// Use this for sign in
 	public void SignIn () {
 		Debug.Log ("start game button is clicked.");
@@ -54,5 +55,66 @@ public class JProcesserSignIn :  MonoBehaviour
 
 		//yield retun aop;
 	}
+
+
+
+	public class JInnerProcesserSignIn : IProcessor
+	{
+
+		protected JInnerProcesserSignIn() {}
+
+		public virtual void run(IDataSet dataSet)
+		{
+		}
+
+		#region 私有方法
+
+		protected bool SendToServer(JObj_SignIn obj)
+		{
+			try {
+				JNetworkDataOperator.SendData(JPacketType.npt_signin_req, obj);
+				return true;
+			} catch (Exception e) {
+				JLog.Debug ("发送数据失败");
+				JLog.Error (e.Message);
+				return false;
+			}
+		}
+		#endregion
+	}
+
+	public class JProcesserSignInSet :  JInnerProcesserSignIn
+	{
+		public override void run(IDataSet dataSet)
+		{
+			IStreamObj obj = dataSet.getData (JObjectType.sign_in);
+			JObj_SignIn signInObj = obj as JObj_SignIn;
+			if (signInObj == null)
+				return;
+			SendToServer (signInObj);
+		}
+	}
+
+	public class JProcesserSignInGet: JInnerProcesserSignIn
+	{
+		public delegate void ToSignIn();
+		public ToSignIn toSignIn;
+
+		public override void run(IDataSet dataSet)
+		{
+			IStreamObj obj = dataSet.getData (JObjectType.sign_in_ret);
+			if (null == obj || null == (obj as JObj_SignRet))
+				JLog.Error ("JProcesserSignInGet : obj is empty!");
+
+			if ((obj as JObj_SignRet).Result == false)
+				JLog.Info ("Received JObj_SignRet but account and code is not registed!");
+			//todo:...remind to regist
+			else
+				if (null != toSignIn)
+					toSignIn ();
+
+		}
+	}
+
 }
 
