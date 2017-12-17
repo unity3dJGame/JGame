d47d75217a492216d956c0a7c7548be58c938125
jqh <2423246342@qq.com>
2017-12-17 21:16:57 +0800

    add JGame.DB and add mysqlaccess

---
+++ JGame/JDB/JDBAccess.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+
+namespace JGame.DB
+{
+	public enum JDBType
+	{
+		Unknown = 0,
+		MySql = 1,
+	}
+	
+	public interface JDBAccess
+	{
+		string DatabaseName {
+			get;
+		}
+
+		string Datasource {
+			get;
+		}
+
+		string Port {
+			get;
+		}
+
+		string UserID {
+			get;
+		}
+
+		string UserPassword {
+			get;
+		}
+
+
+
+		bool Connected
+		{
+			get;
+		}
+
+		IDbConnection DBConnection
+		{
+			get;
+		}
+
+		JDBType DBType();
+			
+		bool Open();
+
+		bool Close();
+
+		bool DoSql(string sql, ref string msg);
+
+		DataSet Select(string sql);
+	}
+}
+
