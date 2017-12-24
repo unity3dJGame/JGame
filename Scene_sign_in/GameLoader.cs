c8c08a0eaa1988b475812a5f0324f6d1808c7b35
jqh <2423246342@qq.com>
2017-12-24 20:01:41 +0800

    modif server ip parse by domain

--- Scene_sign_in/GameLoader.cs
+++ Scene_sign_in/GameLoader.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using JGame;
+using System.Net;
 
 public class GameLoader : MonoBehaviour {
 	public bool IsServer = false;
-	public string ServerIP = "192.168.1.101";
-	public int ServerPort = 9796;
+	public string ServerDomainName = "justonebit.vicp.io";
+	public int ServerPort = 22486;
+	//public string ServerIP = "192.168.1.101";
+	//public int ServerPort = 9796;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +27,19 @@ public class GameLoader : MonoBehaviour {
 
 	public void InitializeManagers()
 	{
-		JGame.JGameManager.SingleInstance.initialize (IsServer, ServerIP, ServerPort);
+		IPAddress address = null;
+		try
+		{
+			IPHostEntry hostEntry = Dns.GetHostEntry (ServerDomainName);
+			address = hostEntry.AddressList [0];
+		}
+		catch (Exception e) {
+			JLog.Error (e.Message, JGame.Log.JLogCategory.Network);
+			return;
+		}
+		if (null == address)
+			return;
+
+		JGame.JGameManager.SingleInstance.initialize (IsServer, address.ToString(), ServerPort);
 	}
 }
